eb3132c471d00f241f4bb07bfb13483fc5e5f4e1
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-13 14:50:27 +0200

    2. Graphic Editor

---
+++ 01. SOLID/01. SOLID - Lab/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P02.Graphic_Editor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var shapes = new IShape[]
+            {
+                new Circle(),
+                new Rectangle(),
+                new Square(),
+            };
+
+            var graphicEditor = new GraphicEditor();
+
+            foreach (var shape in shapes)
+            {
+                graphicEditor.DrawShape(shape);
+            }
+        }
+    }
+}
