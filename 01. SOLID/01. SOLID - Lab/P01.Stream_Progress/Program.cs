b60e5a547ff4e9a8ee9ff3d63a414d26eea0d129
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-13 14:42:16 +0200

    1. Stream Progress Info

---
+++ 01. SOLID/01. SOLID - Lab/P01.Stream_Progress/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        public static void Main()
+        {   
+            IStreamable file = new File("Filename", 100, 50);
+            IStreamable music = new Music("T3", "T4", 100, 30);
+            Music music2 = new Music("Second Artist", "Second Song", 500, 145);
+            File file2 = new File("filename2", 500, 150);
+
+            var streamProgress = new StreamProgressInfo(file);
+            streamProgress = new StreamProgressInfo(music);
+            streamProgress = new StreamProgressInfo(music2);
+            streamProgress = new StreamProgressInfo(file2);
+        }
+    }
+}
