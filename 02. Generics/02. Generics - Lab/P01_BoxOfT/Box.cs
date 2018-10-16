c9dd5f11394b1bd7d5ba8dea2ffc5ba544c0e116
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-16 14:02:41 +0200

    P01_BoxOfT

---
+++ 02. Generics/02. Generics - Lab/P01_BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_BoxOfT
+{
+    using System.Collections.Generic;
+
+    public class Box <T> : IBox<T>
+    {
+        private Stack<T> elements;
+
+        public Box()
+        {
+            this.elements = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Push(element);
+        }
+
+        public T Remove()
+        {
+            return this.elements.Pop();
+        }
+
+        public int Count => this.elements.Count;
+    }
+}
