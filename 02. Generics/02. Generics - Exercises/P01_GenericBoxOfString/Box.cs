fcfa40de5935ced10cb3da76b5fb92cd58427224
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-16 15:51:01 +0200

    01. Generic Box of String

---
+++ 02. Generics/02. Generics - Exercises/P01_GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace P01_GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private readonly T element;
+
+        public Box(T element)
+        {
+            this.element = element;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {this.element}";
+        }
+    }
+}
---
+++ 02. Generics/02. Generics - Exercises/P01_GenericBoxOfString/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_GenericBoxOfString
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var boxes = new Box<string>[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+                var currentBox = new Box<string>(inputLine);
+                boxes[i] = currentBox;
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
