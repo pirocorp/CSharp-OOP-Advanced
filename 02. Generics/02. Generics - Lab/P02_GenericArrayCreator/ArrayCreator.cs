03fd87876233bf223f53ccb572ebd8d3848d0978
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-16 14:12:06 +0200

    02. Array Creator

---
+++ 02. Generics/02. Generics - Lab/P02_GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,17 @@
+﻿namespace P02_GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            var result = new T[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                result[i] = item;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 02. Generics/02. Generics - Lab/P02_GenericArrayCreator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P02_GenericArrayCreator
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var strings = ArrayCreator.Create(5, "Pesho");
+            var integers = ArrayCreator.Create(10, 33);
+
+        }
+    }
+}
