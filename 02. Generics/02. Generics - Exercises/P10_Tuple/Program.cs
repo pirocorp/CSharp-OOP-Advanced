1104d0c2d3454dd4b1effb3e7b7e18bec273989c
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-17 11:43:49 +0200

    10. Tuple

---
+++ 02. Generics/02. Generics - Exercises/P10_Tuple/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P10_Tuple
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var name = $"{tokens[0]} {tokens[1]}";
+            var address = tokens[2];
+
+            var tuple1 = new Tuple<string, string>(name, address);
+
+            tokens = Console.ReadLine().Split();
+            name = tokens[0];
+            var litersOBeer = int.Parse(tokens[1]);
+
+            var tuple2 = new Tuple<string, int>(name, litersOBeer);
+
+            tokens = Console.ReadLine().Split();
+            var integer = int.Parse(tokens[0]);
+            var doubleParameter = double.Parse(tokens[1]);
+
+            var tuple3 = new Tuple<int, double>(integer, doubleParameter);
+
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ 02. Generics/02. Generics - Exercises/P10_Tuple/Tuple.cs
@@ -0,0 +1,20 @@
+﻿namespace P10_Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        public Tuple(T1 item1, T2 item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T1 Item1 { get; private set; }
+
+        public T2 Item2 { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
