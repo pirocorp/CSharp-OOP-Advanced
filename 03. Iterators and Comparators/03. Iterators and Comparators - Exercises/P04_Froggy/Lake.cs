6233bca70d5cbce3fce2002d47bf180ea3c808c4
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-18 17:25:06 +0200

    04. Froggy

---
+++ 03. Iterators and Comparators/03. Iterators and Comparators - Exercises/P04_Froggy/Lake.cs
@@ -0,0 +1,63 @@
+﻿namespace P04_Froggy
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> odds;
+        private Stack<int> evens;
+
+        public Lake(IEnumerable<int> collection)
+        {
+            this.odds = new List<int>();
+            this.evens = new Stack<int>();
+
+            this.ProcessInput(collection);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (var odd in this.odds)
+            {
+                yield return odd;
+            }
+
+            foreach (var even in this.evens)
+            {
+                yield return even;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return $"{string.Join(" ", this.odds)} {string.Join(" ", this.evens)}";
+        }
+
+        private void ProcessInput(IEnumerable<int> collection)
+        {
+            var isEven = false;
+
+            foreach (var item in collection)
+            {
+                if (isEven)
+                {
+                    this.evens.Push(item);
+                    isEven = !isEven;
+                }
+                else
+                {
+                    this.odds.Add(item);
+                    isEven = !isEven;
+                }
+            }
+        }
+    }
+}
---
+++ 03. Iterators and Comparators/03. Iterators and Comparators - Exercises/P04_Froggy/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace P04_Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(numbers);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
