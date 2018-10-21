8890a4986c5426f4f3f6b4b315987307462353de
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-21 11:03:03 +0200

    01. Stealer 02. High Quality Mistakes

---
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Lab/P01_Stealer/Spy.cs
@@ -0,0 +1,78 @@
+﻿namespace P01_Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] nameOfFields)
+        {
+            var result = new StringBuilder();
+
+            var investigatedClass = this.GetClassType(className);
+            var instanceOfInvestigatedClass = Activator.CreateInstance(investigatedClass);
+
+            result.AppendLine($"Class under investigation: {className}");
+
+            var fields = investigatedClass.GetFields(BindingFlags.NonPublic | BindingFlags.Instance
+                                                     | BindingFlags.Static | BindingFlags.Public);
+
+            foreach (var fieldInfo in fields)
+            {
+                var fieldName = fieldInfo.Name;
+
+                if (nameOfFields.Contains(fieldName))
+                {
+                    var fieldValue = fieldInfo.GetValue(instanceOfInvestigatedClass);
+                    result.AppendLine($"{fieldName} = {fieldValue}");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            var result = new StringBuilder();
+
+            var investigatedClass = this.GetClassType(className);
+
+            var publicFields = investigatedClass.GetFields();
+            foreach (var field in publicFields)
+            {
+                result.AppendLine($"{field.Name} must be private!");
+            }
+
+            var properties = investigatedClass.GetProperties(BindingFlags.NonPublic | BindingFlags.Public |
+                                                                  BindingFlags.Instance | BindingFlags.Static);
+
+            var nonPublicGetters = properties
+                .Where(p => !p.GetMethod.IsPublic)
+                .ToArray();
+            foreach (var getter in nonPublicGetters)
+            {
+                result.AppendLine($"get_{getter.Name} have to be public!");
+            }
+
+            var publicSetters = properties
+                .Where(p => p.SetMethod.IsPublic)
+                .ToArray();
+            foreach (var setter in publicSetters)
+            {
+                result.AppendLine($"set_{setter.Name} have to be private!");
+            }
+
+            return result.ToString();
+        }
+
+        private Type GetClassType(string classToInvestigate)
+        {
+            var namespaceAsString = this.GetType().Namespace;
+            var investigatedClass = Type.GetType($"{namespaceAsString}.{classToInvestigate}");
+
+            return investigatedClass;
+        }
+    }
+}
---
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Lab/P01_Stealer/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace P01_Stealer
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.AnalyzeAccessModifiers(nameof(Hacker));
+            Console.WriteLine(result);
+        }
+    }
+}
