f5d92244e010f5d10351b8e49224d03a477d7707
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-21 14:44:36 +0200

    06. Code Tracker

---
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Lab/P05_CreateAttribute/Tracker.cs
@@ -0,0 +1,34 @@
+﻿namespace P05_CreateAttribute
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Attributes;
+
+    [SoftUni("Zdravko")]
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            var softUniType = typeof(SoftUniAttribute);
+
+            var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance |
+                                          BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                var methodCustomAttributes = method.CustomAttributes;
+
+                if (methodCustomAttributes.Any(n => n.AttributeType == softUniType))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (SoftUniAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
