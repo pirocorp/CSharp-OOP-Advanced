1614aa200881fe8d998407fe583ca0f434641b4d
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-22 12:41:47 +0200

    01. Harvesting Fields

---
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,103 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            string inputLine;
+
+            while ((inputLine = Console.ReadLine()) != "HARVEST")
+            {
+                switch (inputLine)
+                {
+                    case "private":
+                        PrintPrivateFields();
+                        break;
+                    case "protected":
+                        PrintProtectedFields();
+                        break;
+                    case "public":
+                        PrintPublicFields();
+                        break;
+                    case "all":
+                        PrintAllFields();
+                        break;
+                }
+            }
+        }
+
+        private static void PrintAllFields()
+        {
+            var publicFields = typeof(HarvestingFields)
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+
+            PrintFields(publicFields);
+        }
+
+        private static void PrintPublicFields()
+        {
+            var publicFields = typeof(HarvestingFields)
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public)
+                .ToArray();
+
+            PrintFields(publicFields);
+        }
+
+        private static void PrintProtectedFields()
+        {
+            var protectedFields = typeof(HarvestingFields)
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic)
+                .Where(f => f.IsFamily)
+                .ToArray();
+
+            PrintFields(protectedFields);
+        }
+
+        private static void PrintPrivateFields()
+        {
+            var privateFields = typeof(HarvestingFields)
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic)
+                .Where(f => f.IsPrivate)
+                .ToArray();
+
+            PrintFields(privateFields);
+        }
+
+        private static void PrintFields(FieldInfo[] protectedFields)
+        {
+            foreach (var field in protectedFields)
+            {
+                var accessModifier = GetAccessModifier(field);
+                Console.WriteLine($"{accessModifier} {field.FieldType.Name} {field.Name}");
+            }
+        }
+
+        private static string GetAccessModifier(FieldInfo field)
+        {
+            var accessModifier = string.Empty;
+
+            if (field.IsFamily)
+            {
+                accessModifier = "protected";
+            }
+
+            if (field.IsPrivate)
+            {
+                accessModifier = "private";
+            }
+
+            if (field.IsPublic)
+            {
+                accessModifier = "public";
+            }
+
+            return accessModifier;
+        }
+    }
+}
