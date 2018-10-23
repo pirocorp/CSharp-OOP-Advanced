7b441d6047f0e1ecc16cc4c62089a5cbaa26958e
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-23 12:23:00 +0200

    Step 5. Card CompareTo()

--- 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/CardPower.cs
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/CardPower.cs
@@ -1,8 +1,9 @@
 ﻿namespace P09_CardGame
 {
+    using System;
     using Enums;
 
-    public class CardPower
+    public class CardPower : IComparable<CardPower>
     {
         public CardPower(CardRank rank, CardSuit suit)
         {
@@ -21,6 +22,22 @@
         public override string ToString()
         {
             return $"Card name: {this.Name}; Card power: {this.Power}";
+        }
+
+
+        public int CompareTo(CardPower other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return 0;
+            }
+
+            if (ReferenceEquals(null, other))
+            {
+                return 1;
+            }
+
+            return this.Power.CompareTo(other.Power);
         }
     }
 }
--- 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/StartUp.cs
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/StartUp.cs
@@ -7,6 +7,27 @@
     {
         public static void Main()
         {
+            var card1 = ReadCardFromConsole();
+            var card2 = ReadCardFromConsole();
+
+            Console.WriteLine(card1);
+            Console.WriteLine(card2);
+            Console.WriteLine();
+
+            var result = card1.CompareTo(card2);
+
+            if (result > 0)
+            {
+                Console.WriteLine(card1);
+            }
+            else
+            {
+                Console.WriteLine(card2);
+            }
+        }
+
+        private static CardPower ReadCardFromConsole()
+        {
             var rankOfCardString = Console.ReadLine();
             var suitOfCardString = Console.ReadLine();
 
@@ -14,7 +35,8 @@
             var suitOfCard = Enum.Parse<CardSuit>(suitOfCardString);
 
             var card = new CardPower(rankOfCard, suitOfCard);
-            Console.WriteLine(card);
+
+            return card;
         }
     }
 }
