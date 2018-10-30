ddd1d2540e4d7d371caa5631d2b3dfef6bfd1f9f
Zdravko Zdravkov <pirocorp@abv.bg>
2018-10-30 13:40:51 +0100

    3.	Command

---
+++ 06. Object Communication and Events/06. Object Communication and Events - Lab/P01_ChainOfResponsibilityCommandDesignPattern/AttackCommand.cs
@@ -0,0 +1,14 @@
+﻿public class AttackCommand : ICommand
+{
+    IAttacker attacker;
+
+    public AttackCommand(IAttacker attacker)
+    {
+        this.attacker = attacker;
+    }
+
+    public void Execute()
+    {
+        this.attacker.Attack();
+    }
+}
