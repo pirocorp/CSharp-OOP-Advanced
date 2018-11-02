224c34ccbcfbf1950de3fabf14ce5e78eb2def7c
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 14:01:01 +0100

    Part VII. Create ViewModels 2.	ContentViewModel

---
+++ 07. Workshops/01_Forum/Forum.App/ViewModels/ContentViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Forum.App.ViewModels
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ContentViewModel
+    {
+        private const int LINE_LENGTH = 37;
+
+        public ContentViewModel(string content)
+        {
+            this.Content = this.GetLines(content);
+        }
+
+        public string[] Content { get; }
+
+        private string[] GetLines(string content)
+        {
+            var contentChars = content.ToCharArray();
+
+            ICollection<string> lines = new List<string>();
+
+            for (var i = 0; i < content.Length; i += LINE_LENGTH)
+            {
+                var row = contentChars.Skip(i).Take(LINE_LENGTH).ToArray();
+                var rowString = string.Join("", row);
+                lines.Add(rowString);
+            }
+
+            return lines.ToArray();
+        }
+    }
+}
