ae2f3e82459542aefad20069266c927e2d9ba77e
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 12:40:45 +0100

    Part II. Create Factories. 1. CommandFactory

--- 07. Workshops/01_Forum/Forum.App/Factories/CommandFactory.cs
+++ 07. Workshops/01_Forum/Forum.App/Factories/CommandFactory.cs
@@ -1,12 +1,50 @@
 ﻿namespace Forum.App.Factories
 {
-	using Contracts;
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Contracts;
 
 	public class CommandFactory : ICommandFactory
 	{
+	    private readonly IServiceProvider serviceProvider;
+
+	    public CommandFactory(IServiceProvider serviceProvider)
+	    {
+	        this.serviceProvider = serviceProvider;
+	    }
+
 		public ICommand CreateCommand(string commandName)
 		{
-			throw new System.NotImplementedException();
+		    var assembly = Assembly.GetExecutingAssembly();
+
+		    var commandType = assembly.GetTypes()
+		        .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+		    if (commandType == null)
+		    {
+		        throw new InvalidOperationException("Command not found!");
+		    }
+
+		    if (!typeof(ICommand).IsAssignableFrom(commandType))
+		    {
+		        throw new InvalidOperationException($"{commandType} is not a {nameof(ICommand)}!");
+		    }
+
+		    var ctor = commandType.GetConstructors().First();
+
+            var ctorParams = ctor.GetParameters();
+		    var args = new object[ctorParams.Length];
+
+		    for (var i = 0; i < args.Length; i++)
+		    {
+		        args[i] = this.serviceProvider.GetService(ctorParams[i].ParameterType);
+		    }
+
+            //var command = (ICommand)ctor.Invoke(args);
+		    var command = Instantiator.CreateInstance<ICommand>(ctor, args);
+            return command;
 		}
-	}
+    }
 }
