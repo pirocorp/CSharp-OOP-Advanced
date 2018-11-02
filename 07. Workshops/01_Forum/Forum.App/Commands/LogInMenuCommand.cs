4d33dc4afe08dd94501832c32f99f961314837e9
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 14:31:10 +0100

    Part VIII. MainMenu 4.	LogInMenuCommand

---
+++ 07. Workshops/01_Forum/Forum.App/Commands/LogInMenuCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace Forum.App.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Contracts;
+
+    public class LogInMenuCommand : ICommand
+    {
+        private IMenuFactory menuFactory;
+
+        public LogInMenuCommand(IMenuFactory menuFactory)
+        {
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            var commandName = this.GetType().Name;
+            var menuName = commandName.Substring(0, commandName.Length - "Command".Length);
+
+            var menu = this.menuFactory.CreateMenu(menuName);
+            return menu;
+        }
+    }
+}
