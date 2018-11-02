00a0085706e970bae6990d15db40a9a9b96cd373
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 18:07:36 +0100

    Part X. ViewCategoryMenu 10.	ViewPostMenuCommand

---
+++ 07. Workshops/01_Forum/Forum.App/Commands/ViewPostMenuCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace Forum.App.Commands
+{
+    using System;
+    using Contracts;
+
+    public class ViewPostMenuCommand : ICommand
+    {
+        private readonly IMenuFactory menuFactory;
+
+        public ViewPostMenuCommand(IMenuFactory menuFactory)
+        {
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            var postId = int.Parse(args[0]);
+
+            var commandName = this.GetType().Name;
+            var menuName = commandName.Substring(0, commandName.Length - "Command".Length);
+
+            var menu = (IIdHoldingMenu)this.menuFactory.CreateMenu(menuName);
+            menu.SetId(postId);
+
+            return menu;
+        }
+    }
+}
