c563476f0d94c325ed2834fd491134bce7f0b623
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 15:19:59 +0100

    Part IX. CategoriesMenu 7.	ViewCategoryMenuCommand

---
+++ 07. Workshops/01_Forum/Forum.App/Commands/ViewCategoryMenuCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace Forum.App.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Contracts;
+
+    public class ViewCategoryMenuCommand : ICommand
+    {
+        private IMenuFactory menuFactory;
+
+        public ViewCategoryMenuCommand(IMenuFactory menuFactory)
+        {
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            var categoryId = int.Parse(args[0]);
+
+            var commandName = this.GetType().Name;
+            var menuName = commandName.Substring(0, commandName.Length - "Command".Length);
+
+            var menu = (IIdHoldingMenu)this.menuFactory.CreateMenu(menuName);
+            menu.SetId(categoryId);
+
+            return menu;
+        }
+    }
+}
