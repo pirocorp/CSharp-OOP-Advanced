ad6c1254bea1f9b8e8d29c0cf9c4e5f02856316c
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 13:17:12 +0100

    Part IV. Create Services 1.	UserService

--- 07. Workshops/01_Forum/Forum.App/Models/Session.cs
+++ 07. Workshops/01_Forum/Forum.App/Models/Session.cs
@@ -1,6 +1,5 @@
 ﻿namespace Forum.App.Models
 {
-    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using Contracts;
---
+++ 07. Workshops/01_Forum/Forum.App/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿namespace Forum.App.Services
+{
+    using System;
+    using Contracts;
+    using Data;
+    using DataModels;
+
+    public class UserService : IUserService
+    {
+        private ForumData forumData;
+        private ISession session;
+
+        public UserService(ForumData forumData, ISession session)
+        {
+            this.forumData = forumData;
+            this.session = session;
+        }
+
+        public User GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserName(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryLogInUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TrySignUpUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
