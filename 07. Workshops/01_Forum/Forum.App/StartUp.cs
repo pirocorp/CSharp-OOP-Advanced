cfbd58949b9e4c2c9290b8ffd9a11befca9c85d7
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 13:44:43 +0100

    Part VI. Create ServicePovider ConfigureServices

--- 07. Workshops/01_Forum/Forum.App/StartUp.cs
+++ 07. Workshops/01_Forum/Forum.App/StartUp.cs
@@ -1,17 +1,19 @@
 ﻿namespace Forum.App
 {
 	using System;
-
 	using Microsoft.Extensions.DependencyInjection;
-
 	using Contracts;
+	using Data;
 	using Factories;
+	using Models;
+	using Services;
 
-	public class StartUp
+    public class StartUp
 	{
 		public static void Main(string[] args)
 		{
-			IMainController menu = new MenuController(new LabelFactory(), new ForumViewEngine());
+		    var serviceProvider = ConfigureServices();
+			var menu = serviceProvider.GetService<IMainController>();
 
 			var engine = new Engine(menu);
 			engine.Run();
@@ -19,7 +21,31 @@
 
 		private static IServiceProvider ConfigureServices()
 		{
-			throw new NotImplementedException();
+			IServiceCollection services = new ServiceCollection();
+
+            //Adding all of Factories as singletons
+		    services.AddSingleton<ITextAreaFactory, TextAreaFactory>();
+		    services.AddSingleton<ILabelFactory, LabelFactory>();
+		    services.AddSingleton<IMenuFactory, MenuFactory>();
+            services.AddSingleton<ICommandFactory, CommandFactory>();
+
+            //Adding ForumData as singleton
+		    services.AddSingleton<ForumData>();
+
+            //Adding Services as transients
+		    services.AddTransient<IPostService, PostService>();
+		    services.AddTransient<IUserService, UserService>();
+
+            //Adding Session, ForumViewEngine and MenuController as singleton
+		    services.AddSingleton<ISession, Session>();
+		    services.AddSingleton<IForumViewEngine, ForumViewEngine>();
+		    services.AddSingleton<IMainController, MenuController>();
+
+            //Adding ForumConsoleReader as transient
+		    services.AddTransient<IForumReader, ForumConsoleReader>();
+
+		    var serviceProvider = services.BuildServiceProvider();
+		    return serviceProvider;
 		}
 	}
 }
