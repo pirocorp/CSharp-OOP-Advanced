3e8a3293d9cfc17976aee31738e13cf52e3724d4
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-02 14:10:49 +0100

    Part VII. Create ViewModels 5.	ReplyViewModel

---
+++ 07. Workshops/01_Forum/Forum.App/ViewModels/ReplyViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Forum.App.ViewModels
+{
+    using Contracts;
+
+    public class ReplyViewModel : ContentViewModel, IReplyViewModel
+    {
+        public ReplyViewModel(string content, string author) 
+            : base(content)
+        {
+            this.Author = author;
+        }
+
+        public string Author { get; }
+    }
+}
