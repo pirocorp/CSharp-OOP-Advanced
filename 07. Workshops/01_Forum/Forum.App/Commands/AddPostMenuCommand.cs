e5ff28fe0c3fa821190f6f3639cbd28923eae596
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-05 12:02:21 +0100

    Part XIII. ViewPostMenu 8.	AddPostMenuCommand

---
+++ 07. Workshops/01_Forum/Forum.App/Commands/AddPostMenuCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Forum.App.Commands
+{
+    using Contracts;
+
+    public class AddPostMenuCommand : ICommand
+    {
+        private readonly IMenuFactory menuFactory;
+
+        public AddPostMenuCommand(IMenuFactory menuFactory)
+        {
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            var commandName = this.GetType().Name;
+            var menuName = commandName.Substring(0, commandName.Length - "Command".Length);
+
+            var menu = this.menuFactory.CreateMenu(menuName);
+            return menu;
+        }
+    }
+}
