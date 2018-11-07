b1991b09939ed792b78d0121cfd0d201b5b48bf5
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-07 13:02:36 +0100

    Part II. Generics, Iterators and Comparators Problem 1. Implementing the SimpleSortedList Data Structure

---
+++ 08. BashSoft/BashSoft/DataStructures/SimpleSortedList.cs
@@ -0,0 +1,129 @@
+﻿namespace BashSoft.DataStructures
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    using Contracts.DataStructures;
+
+    public class SimpleSortedList<T> : ISimpleOrderedBag<T> where T : IComparable<T>
+    {
+        private const int DEFAULT_SIZE = 16;
+
+        private T[] innerCollection;
+        private int size;
+        private readonly IComparer<T> comparison;
+
+        public SimpleSortedList(IComparer<T> comparison, int capacity)
+        {
+            this.comparison = comparison;
+            this.InitializeInnerCollection(capacity);
+        }
+
+        public SimpleSortedList(int capacity) 
+            : this(Comparer<T>.Create((x, y) => x.CompareTo(y)), capacity)
+        {
+        }
+
+        public SimpleSortedList(IComparer<T> comparison)
+            : this(comparison, DEFAULT_SIZE)
+        {
+        }
+
+        public SimpleSortedList()
+            : this(DEFAULT_SIZE)
+        {
+        }
+
+        private void InitializeInnerCollection(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentException("Capacity cannot be negative!");
+            }
+
+            this.innerCollection = new T[capacity];
+        }
+
+        public int Size => this.size;
+
+        public void Add(T element)
+        {
+            if (this.innerCollection.Length <= this.Size)
+            {
+                this.Resize();
+            }
+
+            this.innerCollection[this.Size] = element;
+            this.size++;
+            Array.Sort(this.innerCollection, 0, this.Size, this.comparison);
+        }
+
+        public void AddAll(ICollection<T> collection)
+        {
+            if (this.Size + collection.Count >= this.innerCollection.Length)
+            {
+                this.MultiResize(collection);
+            }
+
+            foreach (var element in collection)
+            {
+                this.innerCollection[this.Size] = element;
+                this.size++;
+            }
+
+            Array.Sort(this.innerCollection, 0, this.Size, this.comparison);
+        }
+
+        public string JoinWith(string joiner)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var element in this)
+            {
+                sb.Append(element);
+                sb.Append(joiner);
+            }
+
+            sb.Remove(sb.Length - joiner.Length, joiner.Length);
+            return sb.ToString();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < this.innerCollection.Length; i++)
+            {
+                yield return this.innerCollection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void Resize()
+        {
+            this.Resize(this.Size * 2);
+        }
+
+        private void MultiResize(ICollection<T> collection)
+        {
+            var newSize = this.innerCollection.Length * 2;
+
+            while (this.Size + collection.Count >= newSize)
+            {
+                newSize *= 2;
+            }
+
+            this.Resize(newSize);
+        }
+
+        private void Resize(int newSize)
+        {
+            var newCollection = new T[newSize];
+            Array.Copy(this.innerCollection, newCollection, this.Size);
+            this.innerCollection = newCollection;
+        }
+    }
+}
