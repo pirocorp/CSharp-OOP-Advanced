a917c7ca70249ec71cff8fd3a7a8c5a8c228ecef
Zdravko Zdravkov <pirocorp@abv.bg>
2018-11-07 17:20:02 +0100

    Part III. Reflection & Attributes Problem 1. Making the Alias Attribute

---
+++ 08. BashSoft/BashSoft/Attributes/AliasAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace BashSoft.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AliasAttribute : Attribute
+    {
+        private readonly string name;
+
+        public AliasAttribute(string name)
+        {
+            this.name = name;
+        }
+        
+        public string Name => this.name;
+
+        public override bool Equals(object obj)
+        {
+            return this.Name.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+    }
+}
