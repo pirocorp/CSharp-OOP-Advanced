2ea882b5efda446ec923d6d9afbb537758e27c31
Zdravko Zdravkov <pirocorp@abv.bg>
2021-06-30 18:11:33 +0200

    Bump up to 3.1

--- 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/Card.cs
+++ 04. Reflection and Attributes/04. Reflection and Attributes - Exercise/P09_CardGame/Card.cs
@@ -2,9 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using Attributes;
     using Enums;
 
     public class Card : IComparable<Card>
